ad02d42f3f6046ecf3c2786cdca3820013e95bc7
Toni Suominen <suomiton@gmail.com>
2015-11-24 22:13:02 +0200

    Added handlebars view cache when in production and enabled port env variable

--- app/server.js
+++ app/server.js
@@ -4,8 +4,15 @@ var routes = require('./configuration/routes');
 var events = require('./configuration/events');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+if(process.env.NODE_ENV === "production") {
+	// Enable handlebars view cache when in production mode
+	app.enable('view cache');
+}
 
 app.get('/', routes.index);
 
@@ -15,7 +22,7 @@ app.disable('etag');
 // Set /static as our static content dir
 app.use('/static', express.static(__dirname + "/static/"));
 
-var server = app.listen(5000, events.express.onStartup);
+var server = app.listen(port, events.express.onStartup);
 
 var io = require('socket.io').listen(server);
 
