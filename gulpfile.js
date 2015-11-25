e6e383a9073f49f5d3851a20f29b4a276b7d2595
Toni Suominen <suomiton@gmail.com>
2015-11-25 15:05:46 +0200

    Minor fixes to build configurations

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@ gulp.task('usemin', function () {
     .pipe(gulp.dest(paths.build + 'views/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function(cb) {
   browserSyncActive = true;
 
   browserSync.init({
